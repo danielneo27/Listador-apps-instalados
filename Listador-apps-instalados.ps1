92a82b1df348c1f71dee74dcdfa4e7352c4af86f
Daniel Neo <78002139+danielneo27@users.noreply.github.com>
2021-01-28 12:13:55 -0300

    v0.2.1 upd2

--- Listador-apps-instalados.ps1
+++ Listador-apps-instalados.ps1
@@ -3,14 +3,14 @@
 	===========================================================================
 	 Criado em:   	16/12/2020 15:59
 	 Criado por:   	Daniel Neo
-     Vers„o:        0.2.1
+     Vers√£o:        0.2.1
      Atualizado:    19/01/2021
 	 Nome do arquivo:  Listador de programas instalados
 	===========================================================================
-	.DESCRI«√O
-		Listador de programas, ele grava todos os programas instalados no PC do usu·rio em um arquivo de texto na pasta Downloads chamado "programas_instalados.txt".
+	.DESCRI√á√ÉO
+		Listador de programas, ele grava todos os programas instalados no PC do usu√°rio em um arquivo de texto na pasta Downloads chamado "programas_instalados.txt".
         
-        Nessa lista ter· informaÁıes sobre o nome do programa e a sua vers„o corresponte.
+        Nessa lista ter√° informa√ß√µes sobre o nome do programa e a sua vers√£o correspondente.
 #>
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
@@ -19,7 +19,7 @@ $host.UI.RawUI.ForegroundColor = "DarkGreen"
 $host.UI.RawUI.BackgroundColor = "Black"
 Clear-Host
 
-Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Sort-Object -Property DisplayName -Unique | Format-Table -AutoSize > ì$env:userprofile\downloads\programas_instalados.txtî
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Sort-Object -Property DisplayName -Unique | Format-Table -AutoSize > ‚Äú$env:userprofile\downloads\programas_instalados.txt‚Äù
 
 #faz uma pausa na quantidade de tempo Em "SEGUNDOS", altere o valor do "Sleep" para funcionar.
 
